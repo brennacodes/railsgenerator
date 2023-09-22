a7eda6e8cd374924d23a0284b5a43eee86d797c4
Brenna Stuart <brennacodes@gmail.com>
2023-09-22 17:32:30 -0600

    Add generators utils for select

---
+++ src/lib/utils/generators.js
@@ -0,0 +1,69 @@
+export const generators = {
+  "Rails": [
+    "application_record",
+    "benchmark",
+    "channel",
+    "controller",
+    "generator",
+    "integration_test",
+    "job",
+    "mailbox",
+    "mailer",
+    "migration",
+    "model",
+    "resource",
+    "scaffold",
+    "scaffold_controller",
+    "system_test",
+    "task"
+  ],
+
+  "ActiveRecord": [
+    "active_record:application_record",
+    "active_record:migration",
+    "active_record:model",
+    "active_record:multi_db"
+  ],
+
+  "Blueprinter": [
+    "blueprinter:blueprint"
+  ],
+
+  "FactoryBot":[
+    "factory_bot:model"
+  ],
+
+  "Rspec": [
+    "rspec:channel",
+    "rspec:controller",
+    "rspec:feature",
+    "rspec:generator",
+    "rspec:helper",
+    "rspec:install",
+    "rspec:integration",
+    "rspec:job",
+    "rspec:mailbox",
+    "rspec:mailer",
+    "rspec:model",
+    "rspec:request",
+    "rspec:scaffold",
+    "rspec:system",
+    "rspec:view"
+  ],
+
+  "TestUnit": [
+    "test_unit:channel",
+    "test_unit:controller",
+    "test_unit:generator",
+    "test_unit:helper",
+    "test_unit:install",
+    "test_unit:integration",
+    "test_unit:job",
+    "test_unit:mailbox",
+    "test_unit:mailer",
+    "test_unit:model",
+    "test_unit:plugin",
+    "test_unit:scaffold",
+    "test_unit:system"
+  ]
+};
