00e50a6cca440b7aec7bc7dd2e3eb96afae70e8e
Brenna Stuart <brennacodes@gmail.com>
2023-09-22 21:36:04 -0600

    Add footer component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,37 @@
+
+<div class="footer">
+  <a class="icon-link" href="www.github.com/brennacodes" target="_blank">
+    <div id="github-logo" alt="GitHub Logo" width="20" height="20"/>
+  </a>
+</div>
+
+<style>
+  .footer {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 6vh;
+    width: 100%;
+    box-sizing: border-box;
+    padding: 0.5rem;
+  }
+
+  .icon-link {
+    box-sizing: border-box;
+    padding: 0.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--bg-color) !important;
+    color: var(--text-color) !important;
+  }
+
+  #github-logo {
+    background-image: var(--gh-icon);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    width: 20px;
+    height: 20px;
+  }
+</style>
